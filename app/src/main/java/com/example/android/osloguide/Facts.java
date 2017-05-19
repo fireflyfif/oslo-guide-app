04cb6aefb1d77197eae0d2f3010e0c56f567d5a0
fireflyfif <fif.iva@gmail.com>
2017-05-19 16:43:06 +0200

    add facts activity

--- app/src/main/java/com/example/android/osloguide/Facts.java
+++ app/src/main/java/com/example/android/osloguide/Facts.java
@@ -1,17 +1,16 @@
 package com.example.android.osloguide;
 
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.ms.square.android.expandabletextview.ExpandableTextView;
 
 public class Facts extends AppCompatActivity {
 
-    private CollapsingToolbarLayout collapsingToolbarLayout;
-
     ExpandableTextView expandableTextView;
-//    ExpandableTextView expandableTextView2;
+    ExpandableTextView expandableTextView2;
+    ExpandableTextView expandableTextView3;
 
 
 
@@ -20,11 +19,24 @@ public class Facts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facts);
 
-        expandableTextView = (ExpandableTextView) findViewById(R.id.expandable_view);
-        expandableTextView.setText(getString(R.string.facts_shopping_about));
+        ((TextView) findViewById(R.id.sample1).findViewById(R.id.fact_title)).
+                setText(getString(R.string.facts_shopping_title));
+        ((TextView) findViewById(R.id.sample2).findViewById(R.id.fact_title)).
+                setText(getString(R.string.facts_osloPass_title));
+        ((TextView) findViewById(R.id.sample3).findViewById(R.id.fact_title)).
+                setText(getString(R.string.facts_emergency_title));
 
-//        expandableTextView2 = (ExpandableTextView) findViewById(R.id.expandable_view2);
-//        expandableTextView2.setText(getString(R.string.facts_osloPass_about));
+        expandableTextView = (ExpandableTextView) findViewById(R.id.sample1).
+                findViewById(R.id.expandable_view);
+        expandableTextView2 = (ExpandableTextView) findViewById(R.id.sample2).
+                findViewById(R.id.expandable_view);
+        expandableTextView3 = (ExpandableTextView) findViewById(R.id.sample3).
+                findViewById(R.id.expandable_view);
+
+        expandableTextView.setText(getString(R.string.facts_shopping_about));
+        expandableTextView2.setText(getString(R.string.facts_osloPass_about));
+        expandableTextView3.setText(getString(R.string.facts_emergency_about));
+
 
     }
 }
